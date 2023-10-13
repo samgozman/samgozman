e78ab03dbc2ba4f22f66985538d63e14c412c07f
samgozman <sam@gozman.space>
2023-10-13 09:09:39 +0400

    Rename

--- src/components/terminal/TerminalComponent.vue
+++ src/components/terminal/TerminalComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import hljs from 'highlight.js/lib/core'
 import bashExtended from './languages/bashExtended'
-import RowTerminal from './RowTerminal.vue'
+import RowBash from './RowBash.vue'
 
 hljs.registerLanguage('bash', bashExtended)
 </script>
@@ -12,7 +12,7 @@ hljs.registerLanguage('bash', bashExtended)
 
 <template>
   <div class="mockup-code">
-    <RowTerminal :hljs="hljs" value="ssh sam@gozman.space:26 -i ~/.ssh/backend_dev_rsa" />
-    <RowTerminal :hljs="hljs" value="curl -s -L https://gozman.space/portfolio.sh | bash" />
+    <RowBash :hljs="hljs" value="ssh sam@gozman.space:26 -i ~/.ssh/backend_dev_rsa" />
+    <RowBash :hljs="hljs" value="curl -s -L https://gozman.space/portfolio.sh | bash" />
   </div>
 </template>

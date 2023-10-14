bfcf158fdfed99cacbfec311c7ec98873aa93019
samgozman <sam@gozman.space>
2023-10-14 14:46:18 +0400

    Add base url for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Note: base for github pages
+  base: process.env.NODE_ENV === 'production' ? '/samgozman/' : '/',
   plugins: [vue()],
   build: {
     sourcemap: true

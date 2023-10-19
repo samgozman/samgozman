5f4600cf46ef9381b1d52bf95f0e9f8b17277530
samgozman <sam@gozman.space>
2023-10-19 11:38:52 +0400

    Remove base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Note: base for github pages
-  base: process.env.NODE_ENV === 'production' ? '/samgozman/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/samgozman/' : '/',
   plugins: [vue()],
   build: {
     sourcemap: true

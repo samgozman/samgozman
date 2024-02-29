b81bedc48bd57e96f776ac605a7d5e23eeeb3184
samgozman <sam@gozman.space>
2024-02-29 11:10:49 +0400

    feat: add base for admin auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,8 +19,43 @@ const router = createRouter({
       path: '/blog/:id',
       name: 'blog-post',
       component: () => import('../views/BlogPostView.vue')
+    },
+    {
+      path: '/admin/login',
+      name: 'admin-login',
+      component: () => import('../views/AdminLoginView.vue')
+    },
+    // TODO: add route to redirect to from GitHub
+    {
+      path: '/admin/posts/new',
+      name: 'admin-new-post',
+      component: () => import('../views/AdminPostNewView.vue'),
+      meta: { requiresAuth: true }
     }
   ]
+})
+
+router.beforeEach(async (to) => {
+  // Check for requiresAuth guard
+  if (!to.meta.requiresAuth) {
+    return
+  }
+
+  // TODO: Add expiration for the stored token
+  // const item = {
+  //   value: value,
+  //   expiry: new Date().getTime() + ttl
+  // }
+  const token = localStorage.getItem('bloggy-token')
+
+  if (
+    !token &&
+    // Avoid an infinite redirect
+    to.name !== 'admin-login'
+  ) {
+    // redirect the user to the login page
+    return { name: 'admin-login', query: { redirect: to.fullPath } }
+  }
 })
 
 export default router
---
+++ src/views/AdminPostNewView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HeaderMax from '@/components/elements/HeaderMax.vue'
+</script>
+
+<template>
+  <HeaderMax value="🔥 Add new post." />
+
+  <div class="w-full">
+    <p>TODO: markdown editor.</p>
+  </div>
+</template>

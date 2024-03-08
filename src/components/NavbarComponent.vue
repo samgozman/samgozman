a54b81ee9c79908e31c2889f482547b25c541a42
samgozman <sam@gozman.space>
2024-03-08 13:09:54 +0400

    chore: menu changes

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -7,25 +7,12 @@ import HeaderSignature from './elements/HeaderSignature.vue'
   <div class="navbar bg-white">
     <div class="navbar-start">
       <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />
-          </svg>
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden pl-0">
+          <ion-icon name="menu-outline" class="text-2xl"></ion-icon>
         </div>
         <ul
           tabindex="0"
-          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          class="menu menu-md dropdown-content mt-2 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-poppins font-medium"
         >
           <li><RouterLink :to="{ name: 'home' }">About</RouterLink></li>
           <li><RouterLink :to="{ name: 'blog' }">Blog</RouterLink></li>
@@ -38,7 +25,7 @@ import HeaderSignature from './elements/HeaderSignature.vue'
       </span>
     </div>
     <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
+      <ul class="menu menu-horizontal px-1 font-poppins font-medium">
         <li><RouterLink :to="{ name: 'home' }">About</RouterLink></li>
         <li><RouterLink :to="{ name: 'blog' }">Blog</RouterLink></li>
         <li><RouterLink :to="{ name: 'projects' }">Projects</RouterLink></li>

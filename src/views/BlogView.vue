e5f13000dee203825f241aca06cc9d29d710e397
samgozman <sam@gozman.space>
2024-03-16 11:27:23 +0400

    feat: fill welcome text

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+import { RouterLink } from 'vue-router'
 import HeaderMax from '@/components/elements/HeaderMax.vue'
 import ArticleProse from '@/components/elements/ArticleProse.vue'
 import BlogListItem from '@/components/bloglist/BlogListItem.vue'
@@ -27,16 +28,19 @@ onMounted(async () => {
   <!-- TODO: Add pagination here -->
 
   <ArticleProse>
-    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    <h2>Welcome to Sam Gozman's Dev Blog!</h2>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor quidem vitae ut nam
-      nisi ea facere, officiis debitis corporis sint expedita qui dolore fugit aut, ad, beatae amet
-      temporibus.
+      Hello fellow developers and enthusiasts! I'm thrilled to welcome you to my blog, a space where
+      I share insights, experiences, and updates from the world of development. As a lead backend
+      engineer with a strong passion for open-source initiatives, I've dedicated myself to crafting
+      and refining my own
+      <RouterLink class="link link-hover" :to="{ name: 'projects' }">projects</RouterLink>. This
+      blog marks the second incarnation of my journey, where I continue to explore, create, and
+      contribute within the vibrant open-source community.
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolor quidem vitae ut nam
-      nisi ea facere, officiis debitis corporis sint expedita qui dolore fugit aut, ad, beatae amet
-      temporibus.
+      Whether you're a seasoned developer or just starting out on your coding journey, there's
+      something here for everyone. <strong>Welcome aboard!</strong>
     </p>
   </ArticleProse>
 </template>

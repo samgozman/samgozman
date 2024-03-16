3f75e31ef8ab1495bb0c02d5eb9aad7a389a015a
samgozman <sam@gozman.space>
2024-03-16 14:57:44 +0400

    fix: footer alignment

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,11 +5,13 @@ import SubscribeForm from './SubscribeForm.vue'
 
 <template>
   <footer
-    class="footer container mx-auto max-w-6xl p-4 flex flex-col md:flex-row justify-center md:justify-between items-center"
+    class="footer container mx-auto max-w-6xl p-4 flex flex-col md:flex-row justify-center md:justify-between items-center md:items-end"
   >
     <aside class="order-2 md:order-1">
       <div class="text-center md:text-left">
-        <div class="grid grid-flow-col gap-4 md:gap-0 mb-8 md:mb-0 text-3xl md:text-2xl text-primary">
+        <div
+          class="grid grid-flow-col gap-4 md:gap-0 mb-8 md:mb-0 text-3xl md:text-2xl text-primary"
+        >
           <a href="https://github.com/samgozman" target="_blank" rel="noopener noreferrer">
             <ion-icon name="logo-github"></ion-icon>
           </a>
@@ -28,7 +30,7 @@ import SubscribeForm from './SubscribeForm.vue'
         <p class="my-2 text-xl md:text-sm">Backend Engineer:<br />Golang, NodeJS, Rust.</p>
       </div>
     </aside>
-    <div class="order-3 md:order-2 flex justify-center md:justify-end">
+    <div class="order-3 md:order-2 flex">
       <fieldset class="form-control">
         <h6 class="footer-title text-center text-xl md:text-sm md:text-right px-2">
           Subscribe to my blog

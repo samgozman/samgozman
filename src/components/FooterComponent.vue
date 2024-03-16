8e6911515ddbe2b7e6e7eb59bf2c290280fd0e64
samgozman <sam@gozman.space>
2024-03-16 12:26:59 +0400

    fix: footer menu on mobile

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -9,7 +9,7 @@ import SubscribeForm from './SubscribeForm.vue'
   >
     <aside class="order-2 md:order-1">
       <div class="text-center">
-        <div class="grid grid-flow-col gap-4 text-2xl text-primary">
+        <div class="grid grid-flow-col gap-4 mb-8 md:mb-0 text-3xl md:text-2xl text-primary">
           <a href="https://github.com/samgozman" target="_blank" rel="noopener noreferrer">
             <ion-icon name="logo-github"></ion-icon>
           </a>
@@ -24,18 +24,20 @@ import SubscribeForm from './SubscribeForm.vue'
             <ion-icon name="paper-plane-outline"></ion-icon>
           </a>
         </div>
-        <p class="my-2 text-xl">Sam Gozman</p>
-        <p class="my-2">Backend Engineer:<br />Golang, NodeJS, Rust.</p>
+        <p class="my-2 text-2xl md:text-xl">Sam Gozman</p>
+        <p class="my-2 text-xl md:text-sm">Backend Engineer:<br />Golang, NodeJS, Rust.</p>
       </div>
     </aside>
     <div class="order-3 md:order-2 flex justify-center md:justify-end">
       <fieldset class="form-control">
-        <h6 class="footer-title text-center md:text-right px-2">Subscribe to my blog</h6>
+        <h6 class="footer-title text-center text-xl md:text-sm md:text-right px-2">
+          Subscribe to my blog
+        </h6>
         <SubscribeForm />
       </fieldset>
     </div>
-    <nav class="order-1 md:hidden">
-      <h6 class="footer-title">Blog navigation</h6>
+    <nav class="order-1 md:hidden text-xl mt-6 flex flex-col items-center">
+      <h6 class="footer-title">Navigation</h6>
       <RouterLink class="link link-hover" :to="{ name: 'home' }">About</RouterLink>
       <RouterLink class="link link-hover" :to="{ name: 'blog' }">Blog</RouterLink>
       <RouterLink class="link link-hover" :to="{ name: 'projects' }">Projects</RouterLink>

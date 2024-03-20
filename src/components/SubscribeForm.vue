8d99f4bdcee5d545a8b264c1d3ea238e51c7c980
samgozman <sam@gozman.space>
2024-03-20 21:51:23 +0400

    chore: load hcaptcha on form focus

--- src/components/SubscribeForm.vue
+++ src/components/SubscribeForm.vue
@@ -9,6 +9,7 @@ const hCaptchaKey = import.meta.env.VITE_HCAPTCHA_SITE_KEY
 const email = ref('')
 const token = ref('')
 const captcha = ref<VueHcaptcha>()
+const formClicked = ref(false)
 
 const router = useRouter()
 const emit = defineEmits(['closeModal'])
@@ -46,6 +47,10 @@ const onSubmit = async (event: Event) => {
 
 // Generate a random string
 const inputName = Math.random().toString(36).substring(2, 10)
+
+const onInputFocus = () => {
+  formClicked.value = true
+}
 </script>
 
 <template>
@@ -57,11 +62,13 @@ const inputName = Math.random().toString(36).substring(2, 10)
         placeholder="Enter your email"
         class="input input-bordered join-item"
         v-model="email"
+        @focus="onInputFocus"
       />
       <button class="btn btn-primary join-item text-base-100">Subscribe</button>
     </div>
     <vue-hcaptcha
       ref="captcha"
+      v-if="formClicked"
       :sitekey="hCaptchaKey"
       size="invisible"
       @verify="onCaptchaVerified"

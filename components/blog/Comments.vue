ac7961ab60a92db292f72c0eda059691f50a122e
samgozman <sam@gozman.space>
2024-04-12 11:14:30 +0400

    feat: add post page

---
+++ components/blog/Comments.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{
+  pageUrl: string
+  pageSlug: string
+  pageTitle: string
+}>()
+
+const disqus_config = function (this: any) {
+  this.page.url = props.pageUrl
+  this.page.identifier = props.pageSlug
+  this.page.title = props.pageTitle.slice(0, 200)
+}
+
+;(function () {
+  // DON'T EDIT BELOW THIS LINE
+  var d = document,
+    s = d.createElement('script')
+  s.src = 'https://gozman.disqus.com/embed.js'
+  s.setAttribute('data-timestamp', String(+new Date()))
+  ;(d.head || d.body).appendChild(s)
+})()
+</script>
+
+<template>
+  <div id="disqus_thread" class="my-8"></div>
+</template>
--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,3 +1,48 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const md = ref<string>()
+const createdAt = ref<Date>(new Date())
+const title = ref<string>('')
+
+const pageURL = window.location.href
+
+const route = useRoute()
+
+const slug = route.params.slug as string
+
+const { error } = useAsyncData('blog-post', async (context) => {
+  if (!slug) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Page Not Found'
+    })
+  }
+
+  const res = await BloggyApi.getPostBySlug(slug)
+  if (!res.ok) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'Page Not Found'
+    })
+  }
+
+  md.value = res.content
+  createdAt.value = new Date(res.created_at)
+  title.value = res.title
+})
+
+if (error.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page Not Found'
+  })
+}
+</script>
+
 <template>
-  <p>{{ $route.params.slug }}</p>
+  <div class="max-w-fit">
+    <MarkdownView v-if="md" :value="md" :created-at="createdAt" />
+    <BlogComments :page-url="pageURL" :page-slug="slug" :page-title="title" />
+  </div>
 </template>

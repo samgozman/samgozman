5d5fd01c03b67816d805d6096aa7666db672350d
samgozman <sam@gozman.space>
2024-04-16 21:12:48 +0400

    feat: add subscription pages

---
+++ pages/subscription/success.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Subscription confirmation | Sam Gozman'
+})
+</script>
+
+<template>
+  <ElementsArticleProse>
+    <h2>Thank you for subscribing!</h2>
+    <p>
+      You will receive a confirmation email shortly. Please click the link in the email to confirm
+      your subscription.
+    </p>
+  </ElementsArticleProse>
+</template>

e7c0657ab3bd6429c08b1e3859e685ea5c2b2590
samgozman <sam@gozman.space>
2024-11-14 21:23:06 +0400

    fix: fix sudden gaps with Satori renderer

--- components/OgImage/BlogPost.vue
+++ components/OgImage/BlogPost.vue
@@ -11,18 +11,18 @@ withDefaults(
 
 <template>
   <div
-    class="h-full w-full flex items-start justify-start bg-white relative text-[#1F2937] og-preview"
+    class="h-full w-full flex items-start justify-start bg-white relative text-[#1F2937]"
     style="font-family: 'Poppins', sans-serif"
   >
-    <h1 class="w-full text-7xl p-20 font-black text-left">
+    <h1 class="w-full text-7xl p-20 font-black text-left gap-x-0">
       {{ title }}
     </h1>
     <div class="w-full text-4xl font-bold px-20 absolute bottom-36 left-0 flex justify-between">
-      <span
+      <span class="gap-x-0"
         >https<span class="text-orange-500">://</span>gozman<span class="text-orange-500">.</span
         >space</span
       >
-      <span>Sam Gozman<span class="text-orange-500 mx-1">|</span>Blog</span>
+      <span class="gap-x-0">Sam Gozman<span class="text-orange-500 mx-1">|</span>Blog</span>
     </div>
     <div class="w-full h-8 bg-orange-500 absolute bottom-20 left-0"></div>
   </div>
--- components/OgImage/Default.vue
+++ components/OgImage/Default.vue
@@ -16,8 +16,8 @@ withDefaults(
   >
     <h1 class="w-full text-7xl px-20 pt-20 font-black text-left">Sam Gozman</h1>
     <div class="w-full text-4xl px-20 mt-[-4rem] text-left">
-      <p class="mb-0">Backend Developer<span class="text-orange-500">.</span></p>
-      <p class="mt-0">
+      <p class="mb-0 gap-x-0">Backend Developer<span class="text-orange-500">.</span></p>
+      <p class="mt-0 gap-x-0">
         Golang<span class="text-orange-500 mr-1">,</span> NodeJS<span class="text-orange-500 mr-1"
           >,</span
         >
@@ -25,11 +25,11 @@ withDefaults(
       </p>
     </div>
     <div class="w-full text-5xl font-bold px-20 absolute bottom-36 left-0 flex justify-between">
-      <span
+      <span class="gap-x-0"
         >https<span class="text-orange-500">://</span>gozman<span class="text-orange-500">.</span
         >space</span
       >
-      <span class="text-orange-500">{{ title }}</span>
+      <span class="text-orange-500 gap-x-0">{{ title }}</span>
     </div>
     <div class="w-full h-8 bg-orange-500 absolute bottom-20 left-0"></div>
   </div>

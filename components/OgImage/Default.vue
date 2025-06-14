f5a17fb65ba9127fdcccf0f6933bf5ceffd38602
samgozman <sam@gozman.space>
2025-06-14 23:31:58 +0400

    feat: update ogImage

--- components/OgImage/Default.vue
+++ components/OgImage/Default.vue
@@ -16,7 +16,7 @@ withDefaults(
   >
     <h1 class="w-full text-7xl px-20 pt-20 font-black text-left">Sam Gozman</h1>
     <div class="w-full text-4xl px-20 mt-[-4rem] text-left">
-      <p class="mb-0 gap-x-0">Backend Developer<span class="text-orange-500">.</span></p>
+      <p class="mb-0 gap-x-0">Senior Backend Engineer<span class="text-orange-500">.</span></p>
       <p class="mt-0 gap-x-0">
         Golang<span class="text-orange-500 mr-1">,</span> NodeJS<span class="text-orange-500 mr-1"
           >,</span

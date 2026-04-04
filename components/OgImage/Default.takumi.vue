0aff838842bcf29a2f795bc7a3e21a655126d0f1
samgozman <sam@gozman.space>
2026-04-04 09:06:54 +0400

    chore: fix og-image takumi rendering

--- components/OgImage/Default.takumi.vue
+++ components/OgImage/Default.takumi.vue
@@ -11,18 +11,21 @@ withDefaults(
 
 <template>
   <div
-    class="h-full w-full flex-col items-start justify-start bg-white relative text-[#1F2937]"
+    class="h-full w-full flex flex-col items-start justify-start bg-white relative text-[#1F2937]"
     style="font-family: 'Poppins', sans-serif"
   >
-    <h1 class="w-full text-7xl px-20 pt-20 font-black text-left">Sam Gozman</h1>
-    <div class="w-full text-4xl px-20 mt-[-4rem] text-left">
-      <p class="mb-0 gap-x-0">Senior Backend Engineer<span class="text-orange-500">.</span></p>
-      <p class="mt-0 gap-x-0">
-        Golang<span class="text-orange-500 mr-1">,</span> NodeJS<span class="text-orange-500 mr-1"
-          >,</span
-        >
-        Rust<span class="text-orange-500">.</span>
-      </p>
+    <div class="w-full flex flex-col px-20 pt-20">
+      <div class="text-7xl font-black text-left gap-x-0">Sam Gozman</div>
+      <div class="text-4xl text-left flex flex-col gap-x-0">
+        <span class="gap-x-0">Senior Backend Engineer<span class="text-orange-500">.</span></span>
+        <span class="gap-x-0">
+          Golang<span class="text-orange-500 mr-1">,</span> NodeJS<span
+            class="text-orange-500 mr-1"
+            >,</span
+          >
+          Rust<span class="text-orange-500">.</span>
+        </span>
+      </div>
     </div>
     <div class="w-full text-5xl font-bold px-20 absolute bottom-36 left-0 flex justify-between">
       <span class="gap-x-0"
